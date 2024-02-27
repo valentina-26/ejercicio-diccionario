73e7f78a85000357a2fb7cc81386c56e03fe97c1
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-02-27 17:02:41 -0500

    Ejercicio diccionarios

--- Tiendita.py
+++ Tiendita.py
@@ -1,49 +1,131 @@
-productos_pan = {
-    "pan_integral": {"precio": 1.5, "descripcion": "Pan integral"},
-    "pan_frances": {"precio": 1.0, "descripcion": "Pan francés"},
-    "pan_molde": {"precio": 2.0, "descripcion": "Pan de molde"}
-}
+eleccion_principal=input("bienvenido a la panaderia kreisel supra,por favor elija el tipo de producto que desea adquirir:pan, bolleria o reposteria?")
+if eleccion_principal == "pan":
+    productos_panaderia =tuple((
+        "baguette",
+        "pan de molde",
+        "pan integral",
+        "pan de centeno",
+        "bagel",
+        "pan de ajo",
+        "panecillos",
+        "pan de hamburguesa",
+        "pan de pita",
+        "panecillos de leche",
+        "promocion pan de ajo + bagel",
+        "promocion bagel + panecillos de leche"
+        ))
+    precios_panaderia=tuple((
+        4000,
+        3000,
+        2500,
+        2500,
+        3200,
+        2500,
+        3000,
+        2200,
+        3000,
+        2800,
+        4000,
+        4000
+            ))
+    for i,val in enumerate(productos_panaderia):
+       print(f"""{i}.{val} ${precios_panaderia[i]}""")
 
-productos_pasteles = {
-    "pastel_chocolate": {"precio": 15.0, "descripcion": "Pastel de chocolate"},
-    "pastel_vainilla": {"precio": 12.0, "descripcion": "Pastel de vainilla"},
-    "pastel_zanahoria": {"precio": 18.0, "descripcion": "Pastel de zanahoria"}
-}
+    eleccion=int(input())
+    num_productos=int(input("cuantos desea?"))
+    print(f"usuario usted selecciono el producto {productos_panaderia[eleccion]}con un valor de ${precios_panaderia[eleccion]*num_productos}")
+    dinero=int(input("escriba la cantidad de dineo con la que cuenta: $"))
+    vueltos=dinero - (precios_panaderia[eleccion]*num_productos)
+    falta=(precios_panaderia[eleccion]*num_productos)-dinero
+    if dinero >= precios_panaderia[eleccion]*num_productos:
+       print("si le alcanza para el producto que desea,sus vueltas son $", vueltos)
+    else:
+       print("no le alcanza para el producto que desea,le faltan $",falta)
 
-productos_bolleria = {
-    "croissant": {"precio": 1.8, "descripcion": "Croissant"},
-    "donuts": {"precio": 1.2, "descripcion": "Donuts"},
-    "ensaimada": {"precio": 2.5, "descripcion": "Ensaimada"}
-}
+elif eleccion_principal == "bolleria":
+    productos_bolleria=tuple(("""
+    "Croissant",
+    "ensaimada",
+    "napolitana de crema",
+    "palmera de chocolate",
+    "palmera de coco"
+    "palmera de crema"
+    "chapata de chocolate"
+    "bollo de leche"
+    "churros", 
+    "rosquillas",
+    "promocion churros + ensaimada",
+    "promocion bollo de leche+palmera de chocolate"
+    """))
+    precios_bolleria = tuple((
+    3000,
+    5000,
+    4800,
+    4000,
+    4000,
+    4000,
+    3500,
+    2500,
+    3000,
+    3200,
+    3500,
+    2000
+    ))
 
-# Solicitar al cliente que elija una categoría
-print("Bienvenido a la panadería kreisel supra, solo por el dia de hoy tenemos promociones en todas nuestras categorias, Por favor elija una categoría de productos:")
-print("1. Panes")
-print("2. Pasteles")
-print("3. Bollería")
-opcion = input("Ingrese el número correspondiente a la categoría: ")
+    for i,val in enumerate(productos_bolleria):
+        print(f"""{i}.{val} ${precios_bolleria}[i]""")
 
-# Mostrar los productos de la categoría seleccionada por el cliente
+    eleccion=int(input())
+    num_productos=int(input("cuantos desea?"))
+    print(f"usuario usted selecciono el producto {productos_bolleria[eleccion]}con un valor de ${precios_bolleria[eleccion]*num_productos}")
+    dinero=int(input("escriba la cantidad de dineo con la que cuenta: $"))
+    vueltos=dinero - (precios_bolleria[eleccion]*num_productos)
+    falta=(precios_bolleria[eleccion]*num_productos)-dinero
+    if dinero >= precios_bolleria[eleccion]*num_productos:
+        print("si le alcanza para el producto que desea,sus vueltas son $", vueltos)
+    else:
+        print("no le alcanza para el producto que desea,le faltan $",falta)
 
-promocion_1=("cualquier producto a tan solo 1.0")
-promocion_2=("cualquier producto a tan solo 10.0") 
-promocion_3=("cualquier producto a tab solo 1.0")
-
-
-if opcion == "1":
-    print("Productos de pan:")
-    for producto, detalles in productos_pan.items():
-        print(producto, "-", detalles["descripcion"], "- Precio:", detalles["precio"])
-    print("promocion:",promocion_1["panes"])
-elif opcion == "2":
-    print("Productos de pasteles:")
-    for producto, detalles in productos_pasteles.items():
-        print(producto, "-", detalles["descripcion"], "- Precio:", detalles["precio"])
-    print("promocion:",promocion_2["pasteles"])
-elif opcion == "3":
-    print("Productos de bollería:")
-    for producto, detalles in productos_bolleria.items():
-        print(producto, "-", detalles["descripcion"], "- Precio:", detalles["precio"])
-    print("promocion:", promocion_3["Bolleria"])
-else:
-    print("Opción no válida. Por favor, seleccione una opción válida (1, 2 o 3).")
+elif eleccion_principal=="reposteria":
+    productos_reposteria=tuple((
+    "tarta de manzana",
+    "Brownie",
+    "mufin",
+    "cookies",
+    "tarta de fresa",
+    "tarta de chocolate",
+    "tarta de limon",
+    "coulante de chocolate",
+    "eclair",
+    "tarta de almendra",  
+    "promocion brownie + cookies",
+    "tarta de almendra + muffin",
+    ))
+    precios_reposteria=tuple((
+    3000,
+    2000,
+    1500,
+    500,
+    4500,
+    4500,
+    4500,
+    5000,
+    3500,
+    4500,
+    5000,
+    5000,
+    ))
+    for i,val in enumerate(productos_reposteria):
+        print(f""" {i}. {val} ${precios_reposteria[i]}""")
+        
+    eleccion=int(input())
+    num_productos=int(input("cuantos desea?"))
+    print(f"usuario usted selecciono el producto {productos_reposteria[eleccion]}con un valor de ${precios_reposteria[eleccion]*num_productos}")
+    dinero=int(input("escriba la cantidad de dineo con la que cuenta: $"))
+    vueltos=dinero - (precios_reposteria[eleccion]*num_productos)
+    falta=(precios_reposteria[eleccion]*num_productos)-dinero
+    if dinero >= precios_reposteria[eleccion]*num_productos:
+        print("si le alcanza para el producto que desea,sus vueltas son $", vueltos)
+    else:
+        print("no le alcanza para el producto que desea,le faltan $",falta)
+        
